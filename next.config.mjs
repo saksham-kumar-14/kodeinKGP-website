ad882d9fb1701de625f9e7c2e34c4279bee1bd8b
Saksham Kumar <90552451+saksham-kumar-14@users.noreply.github.com>
2024-10-15 01:39:41 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
-        output: 'export',
         remotePatterns: [
             {
                 protocol: "https",
